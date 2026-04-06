2aa6577565a3a1816af1165cc3c4968e3fa1d2e1
shm1571 <144427116+shm1571@users.noreply.github.com>
2026-04-06 11:55:58 +0200

    change of project folder names for more efficiency

--- mzanzi_builds/settings.py
+++ mzanzi_builds/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'projects',
+    'main.apps.MainConfig',
+    'crispy_forms', 
+    'crispy_bootstrap5'
+
 ]
 
 MIDDLEWARE = [
--- projects/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.starting_page, name="starting-page"), 
-    path("projects", views.projects, name="projects-page"), 
-    path("projects/<slug:slug>", views.project_detail, name="project-detail-page"),
-
-]
