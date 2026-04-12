b916018b41cd355fef75b3930b55e87af2730bef
shm1571 <144427116+shm1571@users.noreply.github.com>
2026-04-12 20:19:03 +0200

    fixed static file issue for deployment

--- mzanzi_builds/settings.py
+++ mzanzi_builds/settings.py
@@ -115,6 +115,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = '/home'
