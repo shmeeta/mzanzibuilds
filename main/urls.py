f88b1b3d27417817dfcdedb14230f3b0c8adf83c
shm1571 <144427116+shm1571@users.noreply.github.com>
2026-04-12 15:40:24 +0200

    final uml class digrams pdf

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('home/', views.home, name="home"),
     path('sign-up/', views.sign_up, name="sign_up"),
     path("logout/", views.logOut, name ="logout"), 
-    path('password_change/', auth_views.PasswordChangeView.as_view(),  name='password_change'),
     path("create-project/", views.create_project, name ="create_post"), 
     path('project/<int:pk>/update', views.update_project, name="update_project"), 
     path('celebration-wall/', views.celebration_wall, name='celebration_wall'), 
